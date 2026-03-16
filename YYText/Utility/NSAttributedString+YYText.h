7127c2436a2321f70de188828f9d0ca1417941c6
chenfeilaksa <chenfei.chang@holla.world>
2026-03-16 11:56:16 +0800

    add userinfo

--- YYText/Utility/NSAttributedString+YYText.h
+++ YYText/Utility/NSAttributedString+YYText.h
@@ -653,6 +653,13 @@ NS_ASSUME_NONNULL_BEGIN
                                                   alignToFont:(UIFont *)font
                                                     alignment:(YYTextVerticalAlignment)alignment;
 
++ (NSMutableAttributedString *)yy_attachmentStringWithContent:(nullable id)content
+                                                  contentMode:(UIViewContentMode)contentMode
+                                               attachmentSize:(CGSize)attachmentSize
+                                                  alignToFont:(UIFont *)font
+                                                    alignment:(YYTextVerticalAlignment)alignment
+                                               attachUserInfo:(NSDictionary *)userInfo;
+
 /**
  Creates and returns an attahment from a fourquare image as if it was an emoji.
  
--- YYText/Utility/NSAttributedString+YYText.m
+++ YYText/Utility/NSAttributedString+YYText.m
@@ -626,6 +626,61 @@ return style. _attr_;
     
     return atr;
 }
++ (NSMutableAttributedString *)yy_attachmentStringWithContent:(id)content
+                                                  contentMode:(UIViewContentMode)contentMode
+                                               attachmentSize:(CGSize)attachmentSize
+                                                  alignToFont:(UIFont *)font
+                                                    alignment:(YYTextVerticalAlignment)alignment
+                                               attachUserInfo:(NSDictionary *)userInfo{
+    NSMutableAttributedString *atr = [[NSMutableAttributedString alloc] initWithString:YYTextAttachmentToken];
+    
+    YYTextAttachment *attach = [YYTextAttachment new];
+    attach.content = content;
+    attach.contentMode = contentMode;
+    attach.userInfo = userInfo
+    [atr yy_setTextAttachment:attach range:NSMakeRange(0, atr.length)];
+    
+    YYTextRunDelegate *delegate = [YYTextRunDelegate new];
+    delegate.width = attachmentSize.width;
+    switch (alignment) {
+        case YYTextVerticalAlignmentTop: {
+            delegate.ascent = font.ascender;
+            delegate.descent = attachmentSize.height - font.ascender;
+            if (delegate.descent < 0) {
+                delegate.descent = 0;
+                delegate.ascent = attachmentSize.height;
+            }
+        } break;
+        case YYTextVerticalAlignmentCenter: {
+            CGFloat fontHeight = font.ascender - font.descender;
+            CGFloat yOffset = font.ascender - fontHeight * 0.5;
+            delegate.ascent = attachmentSize.height * 0.5 + yOffset;
+            delegate.descent = attachmentSize.height - delegate.ascent;
+            if (delegate.descent < 0) {
+                delegate.descent = 0;
+                delegate.ascent = attachmentSize.height;
+            }
+        } break;
+        case YYTextVerticalAlignmentBottom: {
+            delegate.ascent = attachmentSize.height + font.descender;
+            delegate.descent = -font.descender;
+            if (delegate.ascent < 0) {
+                delegate.ascent = 0;
+                delegate.descent = attachmentSize.height;
+            }
+        } break;
+        default: {
+            delegate.ascent = attachmentSize.height;
+            delegate.descent = 0;
+        } break;
+    }
+    
+    CTRunDelegateRef delegateRef = delegate.CTRunDelegate;
+    [atr yy_setRunDelegate:delegateRef range:NSMakeRange(0, atr.length)];
+    if (delegate) CFRelease(delegateRef);
+    
+    return atr;
+}
 
 + (NSMutableAttributedString *)yy_attachmentStringWithEmojiImage:(UIImage *)image
                                                         fontSize:(CGFloat)fontSize {
